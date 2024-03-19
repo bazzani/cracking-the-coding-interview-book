aa4a094eaf353c28fbfd7982825929ec7072650b
Barry Evans <barryevans80@gmail.com>
2024-03-19 15:48:48 +0900

    Add Gradle init project

---
+++ src/test/java/bevans/ctci/MainTest.java
@@ -0,0 +1,14 @@
+package bevans.ctci;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    void shouldRunMain() {
+        // given
+        // when
+        Main.main(new String[]{});
+
+        // then
+    }
+}
